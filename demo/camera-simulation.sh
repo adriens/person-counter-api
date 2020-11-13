23923e8f72ab7c2d2552004a9d8924d91c1d0008
Bertherat Guillaume <bertherat.guillaume@gmail.com>
2020-11-13 14:00:37 +1100

    Made sleeping time a parameter in script

--- demo/camera-simulation.sh
+++ demo/camera-simulation.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+WAITING_TIME=5
+
 while true;
 do
     for file in `ls CAM01/*`
     do
         echo "Copied $file"
         `cp $file ../input/BUREAU_GLIA.jpg`
-        sleep 5s
+        sleep $WAITING_TIME
     done
 done
