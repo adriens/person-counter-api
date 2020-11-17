d57d628eeb703d676d02135ac4b22aad535b04c6
Bertherat Guillaume <bertherat.guillaume@gmail.com>
2020-11-18 07:23:06 +1100

    Temps entre changement de photo randomiser, closes #25

--- demo/camera-simulation.sh
+++ demo/camera-simulation.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-WAITING_TIME=60
 
 while true;
 do
+    WAITING_TIME=$(( ( RANDOM % 60 ) + 40 ))
     for file in `ls CAM01/* | sort -R`
     do
         echo `date "+[%H:%M:%S] Copied $file"`
